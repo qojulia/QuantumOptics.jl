95a278c35dc355621e44d945f79e7b60b3f5faf7
A. Yavorsky <iavorskii.ar@phystech.edu>
2023-06-29 17:39:19 +0300

    Implement DiagStrategy struct to make fine-tuning possible

--- src/spectralanalysis.jl
+++ src/spectralanalysis.jl
@@ -1,6 +1,30 @@
 using Arpack
+import KrylovKit: eigsolve
 
 const nonhermitian_warning = "The given operator is not hermitian. If this is due to a numerical error make the operator hermitian first by calculating (x+dagger(x))/2 first."
+
+mutable struct DiagStrategy{DT}
+    n::Int
+    v0::Union{Nothing, AbstractVector}
+end
+DiagStrategy{T}(n::Int) where T = DiagStrategy{T}(n, nothing)
+const LapackDiag = DiagStrategy{:lapack}
+const ArpackDiag = DiagStrategy{:arpack}
+const KrylovDiag = DiagStrategy{:krylov}
+export LapackDiag, ArpackDiag, KrylovDiag
+
+arithmetic_unary_error = QuantumOpticsBase.arithmetic_unary_error
+DiagStrategy(op::AbstractOperator) = arithmetic_unary_error("DiagStrategy", op)
+function DiagStrategy(op::DataOperator)
+    basis(op) # Checks basis match
+    DiagStrategy(op.data)
+end
+DiagStrategy(m::Matrix) = LapackDiag(size(m)[1], nothing)
+DiagStrategy(m::SparseMatrixCSC) = ArpackDiag(6, rand(eltype(m), size(m)[1]))
+DiagStrategy(m::AbstractMatrix) = ArgumentError("Cannot detect DiagStrategy for array type $(typeof(m))")
+function _assert_starting_vector(ds::DiagStrategy)
+    @assert ds.v0 !== nothing "Starting vector required for $(typeof(ds)) strategy"
+end
 
 """
     eigenstates(op::AbstractOperator[, n::Int; warning=true])
@@ -20,10 +44,10 @@ dense one using `dense(A)`.
 If the given operator is non-hermitian a warning is given. This behavior
 can be turned off using the keyword `warning=false`.
 """
-function eigenstates(op::DenseOpType, n::Int=length(basis(op)); warning=true)
+function eigenstates(op::Operator, ds::LapackDiag; warning=true)
     b = basis(op)
     if ishermitian(op)
-        D, V = eigen(Hermitian(op.data), 1:n)
+        D, V = eigen(Hermitian(op.data), 1:ds.n)
         states = [Ket(b, V[:, k]) for k=1:length(D)]
         return D, states
     else
@@ -33,26 +57,52 @@ function eigenstates(op::DenseOpType, n::Int=length(basis(op)); warning=true)
         perm = sortperm(D, by=real)
         permute!(D, perm)
         permute!(states, perm)
-        return D[1:n], states[1:n]
+        return D[1:ds.n], states[1:ds.n]
     end
 end
 
 """
 For sparse operators by default it only returns the 6 lowest eigenvalues.
 """
-function eigenstates(op::SparseOpType, n::Int=6; warning::Bool=true,
+function eigenstates(op::Operator, ds::ArpackDiag; warning::Bool=true,
         info::Bool=true, kwargs...)
     b = basis(op)
-    # TODO: Change to sparese-Hermitian specific algorithm if more efficient
+    # TODO: Change to sparse-Hermitian specific algorithm if more efficient
     ishermitian(op) || (warning && @warn(nonhermitian_warning))
     info && println("INFO: Defaulting to sparse diagonalization.
         If storing the full operator is possible, it might be faster to do
         eigenstates(dense(op)). Set info=false to turn off this message.")
-    D, V = eigs(op.data; which=:SR, nev=n, kwargs...)
+    if ds.v0 === nothing
+        D, V = eigs(op.data; which=:SR, nev=ds.n, kwargs...)
+    else
+        D, V = eigs(op.data; which=:SR, nev=ds.n, v0=ds.v0, kwargs...)
+    end
     states = [Ket(b, V[:, k]) for k=1:length(D)]
     D, states
 end
 
+function eigenstates(op::Operator, ds::KrylovDiag; warning::Bool=true,
+    info::Bool=true, kwargs...)
+    b = basis(op)
+    ishermitian(op) || (warning && @warn(nonhermitian_warning))
+    info && println("INFO: Defaulting to sparse diagonalization.
+        If storing the full operator is possible, it might be faster to do
+        eigenstates(dense(op)). Set info=false to turn off this message.")
+    _assert_starting_vector(ds)
+    if ds.v0 === nothing
+        D, Vs = eigsolve(op.data, ds.n, :SR; kwargs...)
+    else
+        D, Vs = eigsolve(op.data, ds.v0, ds.n, :SR; kwargs...)
+    end
+    states = [Ket(b, v) for v=Vs]
+    D, states
+end
+
+function eigenstates(op::AbstractOperator, n::Union{Int,Nothing}=nothing; warning=true, kw...)
+    ds = DiagStrategy(op)
+    n !== nothing && (ds.n = n)
+    eigenstates(op, ds; warning=warning, kw...)
+end
 
 """
     eigenenergies(op::AbstractOperator[, n::Int; warning=true])
@@ -67,29 +117,28 @@ More details can be found at
 If the given operator is non-hermitian a warning is given. This behavior
 can be turned off using the keyword `warning=false`.
 """
-function eigenenergies(op::DenseOpType, n::Int=length(basis(op)); warning=true)
-    b = basis(op)
+function eigenenergies(op::Operator, ds::LapackDiag; warning=true)
     if ishermitian(op)
-        D = eigvals(Hermitian(op.data), 1:n)
+        D = eigvals(Hermitian(op.data), 1:ds.n)
         return D
     else
         warning && @warn(nonhermitian_warning)
         D = eigvals(op.data)
         sort!(D, by=real)
-        return D[1:n]
+        return D[1:ds.n]
     end
 end
 
 """
 For sparse operators by default it only returns the 6 lowest eigenvalues.
 """
-eigenenergies(op::SparseOpType, n::Int=6; kwargs...) = eigenstates(op, n; kwargs...)[1]
+eigenenergies(op::Operator, ds::ArpackDiag; kwargs...) = eigenstates(op, ds; kwargs...)[1]
 
-
-arithmetic_unary_error = QuantumOpticsBase.arithmetic_unary_error
-eigenstates(op::AbstractOperator, n::Int=0) = arithmetic_unary_error("eigenstates", op)
-eigenenergies(op::AbstractOperator, n::Int=0) = arithmetic_unary_error("eigenenergies", op)
-
+function eigenenergies(op::AbstractOperator, n::Union{Int,Nothing}=nothing; kw...)
+    ds = DiagStrategy(op)
+    n !== nothing && (ds.n = n)
+    eigenenergies(op, ds; kw...)
+end
 
 """
     simdiag(ops; atol, rtol)
