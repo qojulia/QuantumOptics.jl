88c15cac25a701e27a11466760f71bf7be1fb69b
Ali <a_eghrari@yahoo.com>
2024-05-13 11:15:06 +0100

    Add a hint for Schrödinger solver and make hints appropriate (#386)

--- src/debug.jl
+++ src/debug.jl
@@ -1,12 +1,21 @@
 function __init__()
     if isdefined(Base.Experimental, :register_error_hint)
-
         Base.Experimental.register_error_hint(MethodError) do io, exc, argtypes, kwargs
-            if exc.f == timeevolution.master
-                printstyled(io, "\nHint", color=:green)
-                print(io, ": If your Hamiltonian is time-dependent, then you may want to use master_dynamic instead of master to solve evolution.")
+            if (exc.f == timeevolution.master) && (length(argtypes) >= 3)
+                # Check if the given Hamiltonian is constant.
+                if !(QuantumOpticsBase.is_const(exc.args[3]))
+                    printstyled(io, "\nHint", color=:green)
+                    print(io, ": You are attempting to use a time-dependent Hamiltonian with a solver that assumes constant dynamics. To avoid errors, please use the _dynamic solvers instead, e.g. master_dynamic instead of master_dynamic.")
+                end
+            end
+
+            if (exc.f == timeevolution.schroedinger) && (length(argtypes) >= 3)
+                # Check if the given Hamiltonian is constant.
+                if !(QuantumOpticsBase.is_const(exc.args[3]))
+                    printstyled(io, "\nHint", color=:green)
+                    print(io, ": You are attempting to use a time-dependent Hamiltonian with a solver that assumes constant dynamics. To avoid errors, please use the _dynamic solvers instead, e.g. schroedinger_dynamic instead of schroedinger_dynamic.")
+                end
             end
         end
-
     end
 end
