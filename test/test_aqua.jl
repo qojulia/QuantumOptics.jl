0efb4d4b0876d40454f40ce673862b4812517fe7
Qingyu Qu <2283984853@qq.com>
2025-05-28 22:55:35 +0800

    Manually exclude piracy from QuantumOpticsBase

--- test/test_aqua.jl
+++ test/test_aqua.jl
@@ -4,5 +4,10 @@ using QuantumOptics
 @testset "Quality Assurance" begin
     using Aqua
 
-    Aqua.test_all(QuantumOptics, piracies = (true,))
+    Aqua.test_all(QuantumOptics, piracies = (broken = true,))
+
+    # manual piracy check to exclude dispatches from QuantumOpticsBase
+    phasespace_funcs = [:qfunc, :wigner, :coherentspinstate, :qfuncsu2, :wignersu2]
+    pirates = [pirate for pirate in Aqua.Piracy.hunt(QuantumOptics) if pirate.name ∉ phasespace_funcs]
+    @test isempty(pirates)
 end
